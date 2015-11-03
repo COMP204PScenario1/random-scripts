2918547189fa620a9284e14c4b4129e87be02820
tycho <work.tycho@gmail.com>
2015-11-03 15:53:06 +0000

    Add the create the database permision

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -4,3 +4,5 @@ sudo postgresql-setup initdb;
 sudo systemctl enable postgresql.service;
 sudo systemctl start postgresql.service;
 sudo bash -c "sudo -u postgres psql -c 'CREATE USER localuser; CREATE DATABASE localuser;'";
+
+sudo bash -c "sudo -u postgres psql -c 'ALTER USER localuser CREATEDB;'"; 
