8ca46c67ffe3f6db2618f3bac45daf793acb92a6
tycho <work.tycho@gmail.com>
2015-11-03 14:47:23 +0000

    Fixed

--- postgres-setup.sh
+++ postgres-setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sudo postgresql-setup initdb;
+sudo systemctl enable postgres.service;
 sudo systemctl start postgres.service;
-sudo bash -c 'psql -c \'CREATE USER localuser; CREATE DATABASE localuser\'';
+sudo bash -c "sudo -u postgres psql -c 'CREATE USER localuser; CREATE DATABASE localuser;'";
