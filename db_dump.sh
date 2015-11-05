475a9f117c6affb412ae8538b11028f89bea409d
garrett-may <glsm.channel@gmail.com>
2015-11-05 13:16:36 +0000

    Added timestamp to filename

--- db_dump.sh
+++ db_dump.sh
@@ -1,3 +1,4 @@
+
 export PGPASSWORD="todo_list_app"
-pg_dump -C -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > backup.gz
+pg_dump -C -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > $(date +"%Y-%m-%d-%H-%M-%S").gz
 
