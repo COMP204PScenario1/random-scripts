1ddbf81971abc833392ca9ac6029502da1fc22a6
BuildTools <unconfigured@null.spigotmc.org>
2015-11-05 12:03:41 +0000

    Added install for Expect

--- db_dump.sh
+++ db_dump.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/expect
+spawn sudo yum install expect
+
+send "y\r"
+
+send "y\r"
+
 spawn pg_dump -C -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > backup.gz
 
 send "todo_list\r"
