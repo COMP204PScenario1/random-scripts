541b4ffc330b7c6824284d218227aa26d417cf9d
BuildTools <unconfigured@null.spigotmc.org>
2015-11-05 12:13:31 +0000

    Edited file

--- db_dump.sh
+++ db_dump.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/expect
-spawn sudo yum install expect
+eval spawn sudo yum install expect
 
 send "y\r"
 
 send "y\r"
 
-spawn pg_dump -C -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > backup.gz
+eval spawn pg_dump -C -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > backup.gz
 
 send "todo_list\r"
 
