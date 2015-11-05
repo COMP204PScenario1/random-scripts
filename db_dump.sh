161f29dc713e88f45455d891b0c3b328277b7139
BuildTools <unconfigured@null.spigotmc.org>
2015-11-05 13:47:00 +0000

    Changed from relative path to absolute path to ensure backup location

--- db_dump.sh
+++ db_dump.sh
@@ -1,4 +1,4 @@
 
 export PGPASSWORD="todo_list_app"
-pg_dump -C -Fc -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > $(date +"%Y-%m-%d-%H-%M-%S").gz
+pg_dump -C -Fc -h studtbickers-p.cs.ucl.ac.uk -U todo_list_app todo_list | gzip -c > /home/localuser/$(date +"%Y-%m-%d-%H-%M-%S").gz
 
